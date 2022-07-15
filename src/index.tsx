03f94e3c5595729ead932f983adb88b258be88c5
Schrimpf, Alexander Lawrence <aschrimpf@contractor.usgs.gov>
2022-07-15 14:43:02 -0500

    Added service worker in updated logos/site text

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
@@ -13,3 +14,5 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
